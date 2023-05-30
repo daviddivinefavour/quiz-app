971adbb310904fa4f32585ca61b122f21576f622
Divinefavour David <blazer0022@gmail.com>
2023-05-30 17:42:52 +0100

    can load environment variables into the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuizModule } from './quiz/quiz.module';
-
+import * as Joi from 'joi';
+// import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
-  imports: [QuizModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'env/.env.dev',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false,
+      },
+    }),
+    // SequelizeModule.forRoot({
+    //   dialect: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'divinefavour',
+    //   password: '',
+    //   database: 'quiz_dev',
+    //   models: [],
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
